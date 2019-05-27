9b634ff9420d207a3bbadf02676b38ba2a79a8de
Guillaume Fricker <fricker.g@zoho.com>
2019-05-27 17:33:36 +0200

    Created compass list

--- functions/utils.py
+++ functions/utils.py
@@ -4,9 +4,11 @@ def getPositionTupleOf(array, symbol):
     whereNp = np.where(array == symbol)
     return list(zip(whereNp[0], whereNp[1]))[0]
 
-def getNeigbourOf(array, position):
-    print(position[0] + 1)
-    print ('N =>', array[(position[0]-1)][position[1]])
-    print ('E =>', array[position[0]][(position[1] + 1)])
-    print ('S =>', array[(position[0]+1)][position[1]])
-    print ('W =>', array[position[0]][(position[1] - 1)])
+def getMostSuitableDirection(array, currentDirection, position):
+    north = ('N', array[(position[0]-1)][position[1]])
+    east = ('E', array[position[0]][(position[1] + 1)])
+    south = ('S', array[(position[0]+1)][position[1]])
+    west = ('W', array[position[0]][(position[1] - 1)])
+    compass = list([north, east, south, west])
+    print(compass)
+    
