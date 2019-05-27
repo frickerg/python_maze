42d0f1a2d6082075425c2ac8e900a9aa52c835ca
Amir <amir.khalife@hotmail.com>
2019-05-27 17:06:36 +0200

    Maping method for Characters Type

--- functions/utils.py
+++ functions/utils.py
@@ -3,3 +3,21 @@ import numpy as np
 def getPositionTupleOf(array, symbol):
     whereNp = np.where(array == symbol)
     return list(zip(whereNp[0], whereNp[1]))[0]
+
+
+def switch_probe(argument):
+    if argument == '*':
+        return 0
+    elif argument == ' ':
+        return 1
+    elif argument == 'A':
+        return 2
+    elif argument == 'B':
+        return 3
+
+
+#myList = ['A',' ','B','*']
+
+#for ele in myList:
+   # print (switch_probe(ele))
+    
