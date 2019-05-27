5b16056bf366bb15eb351e46dc2c96cd22bebf2c
Guillaume Fricker <fricker.g@zoho.com>
2019-05-27 15:58:35 +0200

    Added clear screen functionality

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
+from functions import cls
+from time import sleep 
+
 import numpy as np
 np.set_printoptions(threshold=np.inf)
+
+# numpy examples
+a = np.array([1, 2, 3])   # Create a rank 1 array
+print(a.shape)            # Prints "(3,)"
+print(a[0], a[1], a[2])   # Prints "1 2 3"
+a[0] = 5                  # Change an element of the array
+print(a)                  # Prints "[5, 2, 3]"
+
+b = np.array([[1,2,3],[4,5,6]])    # Create a rank 2 array
+print(b.shape)                     # Prints "(2, 3)"
+print(b[0, 0], b[0, 1], b[1, 0])   # Prints "1 2 4"
+
+sleep(0.5)
+cls.clear()
 
 filename = './resources/maze-one.txt'
 setup = list()
@@ -15,19 +32,12 @@ with open(filename) as fp:
 maze = np.array(setup)
 
 print(maze)
-npwhere = np.where(maze == 'A')
-result = list(zip(npwhere[0], npwhere[1]))[0]
-print('Tuple of arrays returned : ', result)
-print('Found A in array[{}][{}]'.format(result[0],result[1]))
-print(maze[result[0]][result[1]])
-
-# numpy examples
-a = np.array([1, 2, 3])   # Create a rank 1 array
-print(a.shape)            # Prints "(3,)"
-print(a[0], a[1], a[2])   # Prints "1 2 3"
-a[0] = 5                  # Change an element of the array
-print(a)                  # Prints "[5, 2, 3]"
-
-b = np.array([[1,2,3],[4,5,6]])    # Create a rank 2 array
-print(b.shape)                     # Prints "(2, 3)"
-print(b[0, 0], b[0, 1], b[1, 0])   # Prints "1 2 4"
+whereA = np.where(maze == 'A')
+whereB = np.where(maze == 'B')
+resultA = list(zip(whereA[0], whereA[1]))[0]
+resultB = list(zip(whereB[0], whereB[1]))[0]
+print('Tuple of arrays returned A : ', resultA)
+print('Tuple of arrays returned B : ', resultB)
+print('Found A in array[{}][{}]'.format(resultA[0],resultA[1]))
+print('Found A in array[{}][{}]'.format(resultB[0],resultB[1]))
+print(maze[resultA[0]][resultA[1]], maze[resultB[0]][resultB[1]])
