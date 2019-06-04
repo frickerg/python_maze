be30dcb5d207cc94dc9e1ff31481b959e60df467
Guillaume Fricker <fricker.g@zoho.com>
2019-06-04 23:54:59 +0200

    Defined looper function

--- main.py
+++ main.py
@@ -18,32 +18,36 @@ with open(filename) as fp:
             mappedChar = utils.map_character(chars[i])
             setup[cnt].append(mappedChar)
 
+def looper(hand):
+    if hand == 'left':
+        last_moving_direction = "N"
+    elif hand == 'right':
+        last_moving_direction = "S"
+
+    maze = np.array(setup)
+    result_a = utils.get_position_tuple_of(maze, 2)
+    result_b = utils.get_position_tuple_of(maze, 3)
+    navigator = Navigator(result_a, result_b, hand)
+
+    while navigator.has_arrived != True:
+        result_a = utils.get_position_tuple_of(maze, 2)
+        coordinates_list = utils.create_coordinates_list(maze, result_a)
+        next_step = navigator.get_most_suitable_moving_direction(maze, coordinates_list, moving_direction=last_moving_direction)
+
+        print("Looking for the closest path...")
+        navigator.move_to(maze, result_a, next_step["coordinates"])
+        last_moving_direction = next_step["direction"]
+        console.clear()
+    
+    return navigator.actual_path
+
 #############
 # First Run #
 #############
-maze = np.array(setup)
-result_a = utils.get_position_tuple_of(maze, 2)
-result_b = utils.get_position_tuple_of(maze, 3)
-navigator = Navigator(result_a, result_b, 'left')
 
-# TODO: ask if the user wants to move with left/right hand (?)
-last_moving_direction = "N"
-
-# TODO: find alternative routes that are shorter!
-while navigator.has_arrived != True:
-    result_a = utils.get_position_tuple_of(maze, 2)
-    coordinates_list = utils.create_coordinates_list(maze, result_a)
-    next_step = navigator.get_most_suitable_moving_direction(maze, coordinates_list, moving_direction=last_moving_direction)
-
-    print("Looking for the closest path...")
-    navigator.move_to(maze, result_a, next_step["coordinates"])
-    last_moving_direction = next_step["direction"]
-    console.clear()
-    #console.prettyprint(maze, navigator.visited_coordinates)
-
-final_path_left = navigator.actual_path
+actual_left = looper('left')
 remove_loose_ends_left = list()
-for element in final_path_left:
+for element in actual_left:
     if element in remove_loose_ends_left:
         for indice in [i for i, x in enumerate(remove_loose_ends_left) if x == element]:
             del remove_loose_ends_left[indice : len(remove_loose_ends_left)]
@@ -53,28 +57,9 @@ for element in final_path_left:
 # Second Run #
 ##############
 
-# TODO: Remove duplicate of code at the start!
-maze = np.array(setup)
-result_a = utils.get_position_tuple_of(maze, 2)
-result_b = utils.get_position_tuple_of(maze, 3)
-navigator = Navigator(result_a, result_b, 'right')
-
-last_moving_direction = "S"
-
-while navigator.has_arrived != True:
-    result_a = utils.get_position_tuple_of(maze, 2)
-    coordinates_list = utils.create_coordinates_list(maze, result_a)
-    next_step = navigator.get_most_suitable_moving_direction(maze, coordinates_list, moving_direction=last_moving_direction)
-
-    print("Looking for the closest path...")
-    navigator.move_to(maze, result_a, next_step["coordinates"])
-    last_moving_direction = next_step["direction"]
-    console.clear()
-    #console.prettyprint(maze, navigator.visited_coordinates)
-
-final_path_right = navigator.actual_path
+actual_right = looper('right')
 remove_loose_ends_right = list()
-for element in final_path_right:
+for element in actual_right:
     if element in remove_loose_ends_right:
         for indice in [i for i, x in enumerate(remove_loose_ends_right) if x == element]:
             del remove_loose_ends_right[indice : len(remove_loose_ends_right)]
@@ -87,7 +72,7 @@ for element in final_path_right:
 maze = np.array(setup)
 result_a = utils.get_position_tuple_of(maze, 2)
 result_b = utils.get_position_tuple_of(maze, 3)
-navigator = Navigator(result_a, result_b, 'right')
+navigator = Navigator(result_a, result_b)
 
 for next_step in remove_loose_ends_left:
     sleep(1 / 30)
@@ -101,7 +86,7 @@ console.success(len(navigator.actual_path))
 maze = np.array(setup)
 result_a = utils.get_position_tuple_of(maze, 2)
 result_b = utils.get_position_tuple_of(maze, 3)
-navigator = Navigator(result_a, result_b, 'right')
+navigator = Navigator(result_a, result_b)
 
 for next_step in remove_loose_ends_right:
     sleep(1 / 30)
