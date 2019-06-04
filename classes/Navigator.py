0b21b08d742afdbb25b4cb96126d4941f32c85ad
Guillaume Fricker <fricker.g@zoho.com>
2019-06-04 23:22:39 +0200

    Improved left hand side algorithm

--- classes/Navigator.py
+++ classes/Navigator.py
@@ -33,7 +33,6 @@ class Navigator:
             return found_items[0]
         return False
 
-    # Work in Progress!
     def get_most_suitable_moving_direction(self, array, compass, moving_direction="None"):
         available = list(
             [
@@ -42,52 +41,20 @@ class Navigator:
                 if array[value] == 1 or array[value] == 3
             ]
         )
-
-        if len(available) == 1:
-            return available[0]
-
-        next_direction = utils.get_next_direction(moving_direction)
-        move_prioritized = self.contains(available, next_direction)
-        if move_prioritized and move_prioritized["coordinates"] not in self.visited_coordinates:
-            return move_prioritized
-
-        move_ambushed = list([entry for entry in available if entry["coordinates"] in self.visited_coordinates])
-
-        available_copy = list.copy(available)
-        for avoider in move_ambushed:
-            available_copy.remove(avoider)
-
-        keep_moving = self.contains(available, moving_direction)
-
-        if len(available) == 1:
-            return available[0]
-        elif len(available_copy) == 0 and move_prioritized:
-            return move_prioritized
-        elif len(available_copy) == 0 and keep_moving:
-            return keep_moving
-
-        if keep_moving and keep_moving["coordinates"] not in self.visited_coordinates:
-            return keep_moving
-
-        # TODO: Inefficient callback, must be removed through optimizing!
-        if len(available_copy) == 1:
-            return available_copy[0]
-
-        # TODO: Inefficient callback, must be removed through optimizing!
-        for i in range(len(available)):
-            if moving_direction == "E" and available[i]["direction"] == "S":
-                return available[i]
-            elif moving_direction == "S" and available[i]["direction"] == "W":
-                return available[i]
-            elif moving_direction == "W" and available[i]["direction"] == "N":
-                return available[i]
-            elif moving_direction == "N" and available[i]["direction"] == "E":
-                return available[i]
-            elif moving_direction == "E" and available[i]["direction"] == "N":
-                return available[i]
-            elif moving_direction == "W" and available[i]["direction"] == "S":
-                return available[i]
-            elif moving_direction == "S" and available[i]["direction"] == "E":
-                return available[i]
-            elif moving_direction == "N" and available[i]["direction"] == "W":
-                return available[i]
+            
+        next_direction = utils.get_next_direction(moving_direction)        
+        # check if there is an outwards corner on hand side
+        for element in available:
+            if element["direction"] == next_direction:
+                return element
+        # check if there is a wall on hand side
+        for element in available:
+            if element["direction"] == moving_direction:
+                return element
+        # check if there is an inwards corner on hand side
+        for element in available:
+            if element["direction"] == utils.get_opposite_direction(next_direction):
+                return element
+        for element in available:
+            if element["direction"] == utils.get_opposite_direction(moving_direction):
+                return element
