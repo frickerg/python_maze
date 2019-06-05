02fab43bf1192a2c8c98cd14495ec47e8df4355e
Guillaume Fricker <fricker.g@zoho.com>
2019-06-05 04:29:04 +0200

    Merging algorithm comin' in hot 🍹

--- functions/console.py
+++ functions/console.py
@@ -27,10 +27,18 @@ def bad_algorithm():
     exit()
 
 
-def success(number_of_steps):
+def success(merged_path_len, left_hand_len, right_hand_len):
+    print()
+    sleep(1)
+    print(colored("left hand algorithm:  {} steps".format(left_hand_len), "red", attrs=["bold"]))
+    print(colored("right hand algorithm: {} steps".format(right_hand_len), "red", attrs=["bold"]))
+    print(colored("merged algorithm:     {} steps".format(merged_path_len), "red", attrs=["bold"]))
+    print()
+    print(colored("Finished the maze in: {} steps".format(merged_path_len), "red", "on_yellow", attrs=["bold"]))
     sleep(2)
-    print(colored("\nMade by Guillaume Fricker and Amir Khalife, 2019", "yellow"))
-    print(colored("The light of past discovery draws me forward. Its shining light guides me to the glory of exploration.", color="green"))
-    print(colored("Finished the maze in {} steps".format(number_of_steps), "red", "on_yellow", attrs=["bold"]))
-    # exit()
-    sleep(5)
+    print()
+    print(colored("Made by Guillaume Fricker and Amir Khalife, 2019", "yellow"))
+    print(colored("The light of past discovery draws me forward.", color="green"))
+    print(colored("Its shining light guides me to the glory of exploration.", color="green"))
+    print()
+    exit()
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import numpy as np
-import itertools
 
 from functions import utils
 from functions import console
+from functions import merge
 from time import sleep
 
 from classes.Navigator import Navigator
@@ -51,14 +51,14 @@ def looper(hand):
     return navigator.actual_path
 
 
-def printing_looper(array):
+def printing_looper(merged_path):
     maze = np.array(setup)
     result_a = utils.get_position_tuple_of(maze, 2)
     result_b = utils.get_position_tuple_of(maze, 3)
     navigator = Navigator(result_a, result_b)
     direction_list = list()
 
-    for next_step in array:
+    for next_step in merged_path:
         sleep(1 / 30)
         console.clear()
         print(next_step)
@@ -67,82 +67,15 @@ def printing_looper(array):
         direction_list.append(next_step)
         console.prettyprint(maze, navigator.visited_coordinates)
 
-    # TODO: Make direction_list contain the actual directions
-    print(direction_list)
-    console.success(len(navigator.actual_path))
-
 
 actual_left = looper("left")
+len_left = len(actual_left)
 clean_left = remove_loose_ends(actual_left)
-printing_looper(clean_left)
 
 actual_right = looper("right")
+len_right = len(actual_right)
 clean_right = remove_loose_ends(actual_right)
-printing_looper(clean_right)
-merged_path = list()
 
-print(None in clean_left)
-for left, right in itertools.zip_longest(clean_left, clean_right):
-    if left == right:
-        merged_path.append(left)
-    else:
-        try:
-            iter_start_left = clean_left.index(left)
-            iter_start_right = clean_right.index(right)
-            iter_end_left = 0
-            iter_end_right = 0
-            steps_left = 0
-            steps_right = 0
-        except ValueError:
-            pass
-        try:
-            if left in clean_right:
-                iter_end_left = clean_right.index(left)
-                print("left", iter_start_left, iter_end_left, "->", iter_start_right, iter_end_right)
-                print("steps", steps_left)
-                steps_left += 1
-            else:
-                pass
-        except ValueError:
-            pass
-        try:
-            if right in clean_left:
-                iter_end_right = clean_left.index(right)
-                print("right", iter_start_right, iter_end_right, "->", iter_start_left, iter_end_left)
-                print("steps", steps_right)
-                steps_right += 1
-            else:
-                pass
-        except ValueError:
-            pass
-"""
-for i in range(len(clean_right)):
-    if clean_left[i] == clean_right[i]:
-        merged_path.append(clean_left)
-    else:
-        steps_left = 0
-        steps_right = 0
-        found_left = False
-        found_right = False
-
-        while found_left == False:
-            try:
-                print(clean_left.index(clean_right[i + steps_right]))
-                print('right',clean_right[i + steps_right])
-                found_left = True
-            except ValueError:
-                pass
-            steps_left += 1
-            steps_right += 1
-        while found_right == False:
-            try:
-                clean_right.index(clean_left[i + steps_left])
-                print('left',clean_left[i + steps_left])
-                found_right = True
-            except ValueError:
-                pass
-            steps_left += 1
-            steps_right += 1
-        print(steps_left, steps_right)
-print(merged_path)
-"""
+merged_path = merge.merge_to_shortest_path(clean_left, clean_right)
+printing_looper(merged_path)
+console.success(len(merged_path), len_left, len_right)
